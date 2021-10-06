fdcec4467b2e8bc060dbe872bcb104976618138b
Unknown <kevin_said@outlook.com>
2021-10-06 00:22:13 -0500

    Chat works! kinda
    Added a post functionality on server side, front end receives text, passes it to server as json, server gets it and returns it as post, needs to reload to see results tho.

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const { response } = require('express')
 var express = require('express')
+// Deprecated, the express function replaces body-parses now and generates the same result
+//var bodyParser = require('body-parser') 
 var app = express()
 
 app.use(express.static(__dirname))
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
 
 var messages = [
     {name: 'Tim', message: 'Hi'},
@@ -11,6 +15,12 @@ var messages = [
 
 app.get('/messages', (req, res) => {
     res.send(messages)
+})
+
+app.post('/messages', (req, res) => {
+    console.log(req.body)
+    messages.push(req.body)
+    res.sendStatus(200)
 })
 
 var server = app.listen(3000, () => {
